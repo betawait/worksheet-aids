2d6b4ed068b24f8d3a7f0fc967e16a69108f1c1a
Stephen Young <40029520+betawait@users.noreply.github.com>
2018-12-15 14:36:07 +0900

    Updates

---
+++ precache-manifest.7e9e69f57ad46533e2f6d0dcaa34766f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7c4d303238f1275963b",
+    "url": "/worksheet-aids/static/js/runtime~main.e7c4d303.js"
+  },
+  {
+    "revision": "5d016e7737d50dbac016",
+    "url": "/worksheet-aids/static/js/main.5d016e77.chunk.js"
+  },
+  {
+    "revision": "b7e68e6126f567802711",
+    "url": "/worksheet-aids/static/js/1.b7e68e61.chunk.js"
+  },
+  {
+    "revision": "5d016e7737d50dbac016",
+    "url": "/worksheet-aids/static/css/main.3145af3d.chunk.css"
+  },
+  {
+    "revision": "bc888602754712da798685dc59295cce",
+    "url": "/worksheet-aids/index.html"
+  }
+];
